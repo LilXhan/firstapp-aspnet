2dbe2e22f76b4545700aa854cb2e0724eb0ea469
Flavio Alvarado Tucto <alvaradotuctod@gmail.com>
2023-07-23 18:39:44 -0500

    first app

---
+++ SystemInventario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemInventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
